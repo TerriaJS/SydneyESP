81d5bba298df0376fd5c9a1429e5e303b8758cf7
Kevin Ring <kevin@kotachrome.com>
2016-11-22 14:58:40 +1100

    Change support email address.

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ var version = require('./version');
 
 var terriaOptions = {
     appName: 'Open Data Explorer',
-    supportEmail: 'steve.bennett@nicta.com.au',
+    supportEmail: 'terriajs@lists.nicta.com.au',
     baseUrl: 'build/TerriaJS',
     cesiumBaseUrl: undefined // use default
 };
